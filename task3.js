3ec923c6b49219cd6c4211ce0700f9d7e2170ccb
Karina_Go <k.gorodnitskaya@gmail.com>
2018-06-17 16:45:38 -0400

    Задание 3

--- task3.js
+++ task3.js
@@ -1,3 +1,18 @@
 /* Task 3 */
 
-/* Put your code here */
+let input = process.argv[2];
+let vowels = '';
+let consonant = '';
+let digits = '';
+
+for (symbol of input) {
+  if ((/^[aeiouy]$/i).test(symbol)) { // гласные
+    vowels += symbol;
+  } else if ((/^[b-df-hj-np-tv-xz]$/i).test(symbol)) { // согласные
+    consonant += symbol;
+  } else if ((/^\d$/i).test(symbol)) { // цифры
+    digits += symbol;
+  }
+}
+
+console.log(vowels + ' ' + consonant + ' ' + digits);
