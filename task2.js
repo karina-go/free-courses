fa6db8e49f24ab004b362b259179d06d54a26293
Karina_Go <k.gorodnitskaya@gmail.com>
2018-06-17 16:23:16 -0400

    Задание 2

--- task2.js
+++ task2.js
@@ -1,3 +1,34 @@
 /* Task 2 */
 
-/* Put your code here */
+/**
+ * Считает элемент ряда Фибоначчи по номеру
+ * @param {number} n номер
+ * @returns {number} элемент
+ */
+function fibonacci(n){
+  // для 0 особый случай
+  if (n === 0) {
+    return 0;
+  }
+
+  let previous = 0;
+  let current = 1;
+
+  // положительные числа
+  for (let i = 1; i < n; i++) {
+    let temp = current;
+    current += previous;
+    previous = temp;
+  }
+
+  // отрицательные числа
+  for (let i = -1; i > n; i--) {
+    let temp = current;
+    current = previous - current;
+    previous = temp;
+  }
+
+  return current;
+}
+
+console.log(fibonacci(parseInt(process.argv[2])));
