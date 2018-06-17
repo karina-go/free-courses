54e6594ce3c4326bc1ef7eb1f7d5b3a05afb6044
Karina_Go <k.gorodnitskaya@gmail.com>
2018-06-17 14:35:06 -0400

    Задание 1

--- task1.js
+++ task1.js
@@ -1,3 +1,24 @@
 /* Task 1 */
 
-/* Put your code here */
+/**
+ * Проверяет, является ли предложение палиндромом
+ * @param {string} sentence
+ * @returns {string} 'YES' если палиндром, 'NO' если нет
+ */
+function isPalindrome(sentence) {
+  const letters = sentence
+    .replace(/^w+/g,'') // оставляем только буквы
+    .toLowerCase() // приводим к нижнему регистру
+
+  // В целях эффективности двигаемся по массиву с разных концов в центр,
+  // сравнивая соответствующие элементы
+  for (let i = 0; i < Math.ceil(letters.length / 2); i++) {
+    if (letters[i] !== letters[letters.length - i - 1]) {
+      return 'NO';
+    }
+  }
+
+  return 'YES';
+}
+
+console.log(isPalindrome(process.argv[2]));
